fa606159119375bcd32a644ef7cb773f410bfab7
Colin Rundel <rundel@gmail.com>
2019-09-25 09:56:14 +0100

    Initial

---
+++ hw1_whitelist.R
@@ -0,0 +1,20 @@
+allowed_files = c("hw2.Rmd",
+                  "README.md",
+                  "wercker.yml",
+                  "hw2.Rproj",
+                  "hw2_whitelist.R",
+                  "fizzbuzz.png",
+                  ".gitignore")
+
+files = dir()
+disallowed_files = files[!(files %in% allowed_files)]
+
+if (length(disallowed_files != 0)) {
+  cat("Disallowed files found:\n")
+  cat("  (remove the following files from your repo)\n\n")
+
+  for (file in disallowed_files)
+    cat("*", file, "\n")
+
+  quit("no", 1, FALSE)
+}
